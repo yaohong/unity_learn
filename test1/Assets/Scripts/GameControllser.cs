712da85d3c27f3ed7b2c8f01bfaddfded3b1fe3f
yaohong <231344781@qq.com>
2017-05-14 19:11:48 +0800

    init

---
+++ test1/Assets/Scripts/GameControllser.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllser : MonoBehaviour {
+
+	// Use this for initialization
+    public GameObject ball;
+    private float maxWidth;
+    private float time = 2;
+    private GameObject newBall;
+	void Start () {
+        Vector3 screenPos = new Vector3(Screen.width, 0, 0);
+        Vector3 moveWidth = Camera.main.ScreenToWorldPoint(screenPos);
+        float ballWidth = ball.GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = moveWidth.x - ballWidth;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            time = Random.Range(1.5f, 2.0f);
+            float posX = Random.Range(-maxWidth, maxWidth);
+            Vector3 spawnPosition = new Vector3(posX, transform.position.y, 0);
+            newBall = (GameObject)Instantiate(ball, spawnPosition, Quaternion.identity);
+            Destroy(newBall, 10);
+        }
+	}
+}
---
+++ test1/Assets/Scripts/SwanMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwanMove : MonoBehaviour {
+
+	// Use this for initialization
+    private float moveSpeed = 4;
+	void Start () {
+        transform.position = new Vector3(22, 3, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (transform.position.x > -22)
+        {
+            transform.Translate(Vector3.right * -moveSpeed * Time.deltaTime);
+        }
+        else 
+        {
+            transform.position = new Vector3(22, 3, 0);
+        }
+	}
+}
